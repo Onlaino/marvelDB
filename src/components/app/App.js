397fc7af789a4be0db158b4b0ff985057d8012ce
Oleg Vasilev <v.oleg2908@gmail.com>
2024-01-25 19:59:32 +0300

    Добавлен React Router и маршрутизация между страницами приложения Немного изменена структура

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,47 +1,30 @@
-// hooks
-import {useState} from "react";
+// react-router-dom
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 // components
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
-
 
 import PropTypes from "prop-types";
 
 
 const App = () => {
-	const [selectedChar, setChar] = useState(null);
-
-
-	const onCharSelected = (id) => {
-		setChar(id);
-	}
-
 	return (
-		<div className="app">
-			<AppHeader/>
-			<main>
-				{/*<ErrorBoundary>*/}
-				{/*	<RandomChar/>*/}
-				{/*</ErrorBoundary>*/}
-				{/*<div className="char__content">*/}
-				{/*	<ErrorBoundary>*/}
-				{/*		<CharList onCharSelected={onCharSelected}/>*/}
-				{/*	</ErrorBoundary>*/}
-				{/*	<ErrorBoundary>*/}
-				{/*		<CharInfo charId={selectedChar}/>*/}
-				{/*	</ErrorBoundary>*/}
-				{/*</div>*/}
-				<ComicsList/>
-				<img className="bg-decoration" src={decoration} alt="vision"/>
-			</main>
-		</div>
+		<Router>
+			<div className="app">
+				<AppHeader/>
+				<Switch>
+					<main>
+						<Route exact path="/">
+							<MainPage/>
+						</Route>
+						<Route exact path="/comics">
+							<ComicsPage/>
+						</Route>
+					</main>
+				</Switch>
+			</div>
+		</Router>
 	)
 
 }
